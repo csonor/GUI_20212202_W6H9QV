3601bd33e54e9529d9eba59c22961e077f34a21d
vass.bence@stud.uni-obuda.hu <vass.bence@stud.uni-obuda.hu>
2022-04-26 11:15:21 +0200

    Játék menü

--- ColorCross/MainWindow.xaml.cs
+++ ColorCross/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ColorCross.WindowFunctions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,29 @@ namespace ColorCross
 		public MainWindow()
 		{
 			InitializeComponent();
-		}
-	}
+
+            Resizer.SetIntials(this);
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            Resizer.Exit();
+        }
+
+        private void max_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Resizer.DoMaximize(this, btn);
+        }
+
+        private void min_Click(object sender, RoutedEventArgs e)
+        {
+            Resizer.Minimize(this);
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+    }
 }
