ab458d753b3a062d03e3ee96a67e39b8f672ddb3
Csongor Dobák <csdobak2@gmail.com>
2022-04-27 15:05:31 +0200

    Changed Color to  system.windows.media namespace

--- ColorCross/Logic/ColorCrossLogic.cs
+++ ColorCross/Logic/ColorCrossLogic.cs
@@ -4,16 +4,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
 
-namespace ColorCross.Backend
+namespace ColorCross.Logic
 {
-	class ColorCrossLogic : IImageRead
+	class ColorCrossLogic
 	{
 		Color[,] pixels;
 		List<Color> colors;
 		LineOfColors[] rows;
 		LineOfColors[] columns;
 		Bitmap bmp;
+
+		public LineOfColors[] Rows { get => rows; }
+		public LineOfColors[] Columns { get => columns; }
+		public List<Color> Colors { get => colors; }
 
 		public ColorCrossLogic()
 		{
@@ -25,17 +34,44 @@ namespace ColorCross.Backend
 			bmp = new Bitmap(fileName);
 			rows = new LineOfColors[bmp.Height];
 			columns = new LineOfColors[bmp.Width];
+
 			for (int i = 0; i < bmp.Height; i++)
 			{
 				for (int j = 0; j < bmp.Width; j++)
 				{
 					var color = bmp.GetPixel(j, i);
-					if (!colors.Contains(color) && color.Name != "0")
-						colors.Add(color);
+					var newColor = Color.FromArgb(color.A, color.R, color.G, color.B);
+					if (!colors.Contains(newColor) && color.Name != "0")
+						colors.Add(newColor);
+				}
+				CountRowColors();
+				CountColumnColors();
+			}
+		}
+
+		public Button[] CreateButtons()
+		{
+			pixels = new Color[bmp.Height, bmp.Width];
+			for (int i = 0; i < pixels.GetLength(0); i++)
+			{
+				for (int j = 0; j < pixels.GetLength(1); j++)
+				{
+					pixels[i, j] = Color.FromArgb(0, 0, 0, 0);
 				}
 			}
-			CountRowColors();
-			CountColumnColors();
+
+			var buttons = new Button[bmp.Width * bmp.Height];
+			for (int i = 0; i < buttons.Length; i++)
+			{
+				buttons[i] = new Button
+				{
+					Name = i.ToString(),
+					Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0)),
+					BorderThickness = new System.Windows.Thickness(0),
+					Command = new RoutedCommand() //TODO button chamge method
+				};
+			}
+			return buttons;
 		}
 
 		void CountRowColors()
@@ -47,6 +83,7 @@ namespace ColorCross.Backend
 					int k = j + 1;
 					int sum = 0;
 					var color = bmp.GetPixel(j, i);
+					var newColor = Color.FromArgb(color.A, color.R, color.G, color.B);
 					while (k < bmp.Width && bmp.GetPixel(k, i) == color)
 					{
 						sum++;
@@ -56,7 +93,7 @@ namespace ColorCross.Backend
 					{
 						if (rows[i].Colors == null)
 							rows[i].Colors = new List<ColorNumber>();
-						rows[i].Colors.Add(new ColorNumber { Color = color, Count = sum + 1 });
+						rows[i].Colors.Add(new ColorNumber { Color = newColor, Count = sum + 1 });
 					}
 					j = k - 1;
 				}
@@ -73,6 +110,7 @@ namespace ColorCross.Backend
 					int k = j + 1;
 					int sum = 0;
 					var color = bmp.GetPixel(i, j);
+					var newColor = Color.FromArgb(color.A, color.R, color.G, color.B);
 					while (k < bmp.Height && bmp.GetPixel(i, k) == color)
 					{
 						sum++;
@@ -82,7 +120,7 @@ namespace ColorCross.Backend
 					{
 						if (columns[i].Colors == null)
 							columns[i].Colors = new List<ColorNumber>();
-						columns[i].Colors.Add(new ColorNumber { Color = color, Count = sum + 1 });
+						columns[i].Colors.Add(new ColorNumber { Color = newColor, Count = sum + 1 });
 					}
 					j = k - 1;
 				}
--- ColorCross/Logic/ColorNumber.cs
+++ ColorCross/Logic/ColorNumber.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
-namespace ColorCross.Backend
+namespace ColorCross.Logic
 {
 	struct ColorNumber
 	{
