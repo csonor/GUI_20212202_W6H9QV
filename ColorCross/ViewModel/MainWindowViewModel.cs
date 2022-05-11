ab6975b567eed019756468c554b1cafaa72fbbd5
vass.bence@stud.uni-obuda.hu <vass.bence@stud.uni-obuda.hu>
2022-05-12 01:48:22 +0200

    merge

--- ColorCross/ViewModel/MainWindowViewModel.cs
+++ ColorCross/ViewModel/MainWindowViewModel.cs
@@ -21,8 +21,6 @@ namespace ColorCross.ViewModel
 		}
 
 
-		public RelayCommand OpenSettingsCommand { get; set; }
-		public SettingsUserControl SettingsView { get; set; }
 
 		public RelayCommand OpenScoreBoardCommand { get; set; }
 		public ScoreBoardUserControl ScoreBoardView { get; set; }
@@ -39,15 +37,12 @@ namespace ColorCross.ViewModel
 		{
 
 			GameMenuView = new LevelSelectUserControl();
-			SettingsView = new SettingsUserControl();
+		
 			HelpView = new HelpUserControl();
 			ScoreBoardView = new ScoreBoardUserControl();
 			currentView = GameMenuView;
 
-			OpenSettingsCommand = new RelayCommand(() =>
-			{
-				CurrentView = SettingsView;
-			});
+		;
 			OpenHelpCommand = new RelayCommand(() =>
 			{
 				CurrentView = HelpView;
